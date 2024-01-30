972cdd2996db3c9bf0ecaca78120f8f459b9bbd4
stevensi33 <michael.acosta@udea.edu.co>
2024-01-30 15:20:28 -0500

    actualización

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ async function manageProducts() {
     await productManager.addProduct({ title: 'Producto 2', description: 'Descripción 2', price: 100, thumbnail: 'ruta2', code: 'code2', stock: 10 });
     console.log(await productManager.getProducts());
 
-    // Suponiendo que quieres buscar el producto con ID 1
+    //buscar el producto con ID 1
     const productId = 1;
-    
-    // Llamada a getProductById para obtener un producto específico por su ID
+
+    // obtener un producto específico por su ID
     const product = await productManager.getProductById(productId);
     if (product) {
         console.log(`Producto encontrado con ID ${productId}:`, product);
